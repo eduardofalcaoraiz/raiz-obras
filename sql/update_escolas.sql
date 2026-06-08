8c0b4728e0cd5a39847c070f6df150a3b4f8614e
eduardofalcaoraiz <eduardo.falcao@raizeducacao.com.br>
2026-06-08 16:01:23 -0300

    sql: update_escolas.sql

---
+++ sql/update_escolas.sql
@@ -0,0 +1,70 @@
+-- GRUPO QI (raiz CNPJ 86.704.160 - QI Qualidade Integral de Ensino)
+UPDATE unidades SET cnpj='86.704.160/0003-07', endereco='Rua da Matriz, 63 - Botafogo, Rio de Janeiro - RJ, 22.260-100' WHERE id='u66';
+UPDATE unidades SET cnpj='86.704.160/0005-60', endereco='Rua Joaquim Pinheiro, 95 - Freguesia, Rio de Janeiro - RJ, 22.743-660' WHERE id='u60';
+UPDATE unidades SET cnpj='33.590.308/0001-93', endereco='Rua Lopes da Cruz, 72 - Méier, Rio de Janeiro - RJ, 20.720-170' WHERE id='u61';
+UPDATE unidades SET cnpj='86.704.160/0009-94', endereco='Rua Odilon Martins de Andrade, 3 - Recreio dos Bandeirantes, Rio de Janeiro - RJ, 22.790-230' WHERE id='u63';
+UPDATE unidades SET cnpj='86.704.160/0008-03', endereco='Estrada Coronel Pedro Correia, 887 - Jacarepaguá, Rio de Janeiro - RJ, 22.775-090' WHERE id='u62';
+UPDATE unidades SET cnpj='86.704.160/0001-37', endereco='Rua Ibituruna, 37 - Tijuca, Rio de Janeiro - RJ' WHERE id='u64';
+UPDATE unidades SET cnpj='86.704.160/0015-32', endereco='Rua Luiz Beltrão, 483 - Vila Valqueire, Rio de Janeiro - RJ, 21.321-230' WHERE id='u65';
+UPDATE unidades SET cnpj='86.704.160/0004-80', endereco='Av. das Américas, 5777 - Barra da Tijuca, Rio de Janeiro - RJ, 22.793-080' WHERE id='u8';
+UPDATE unidades SET cnpj='86.704.160/0003-07', endereco='Rua da Matriz, 63 - Botafogo, Rio de Janeiro - RJ, 22.260-100' WHERE id='u10';
+UPDATE unidades SET cnpj='86.704.160/0005-60', endereco='Rua Joaquim Pinheiro, 95 - Freguesia, Rio de Janeiro - RJ, 22.743-660' WHERE id='u7';
+UPDATE unidades SET cnpj='86.704.160/0013-70', endereco='Estrada Coronel Pedro Correia, 1305 - Jacarepaguá, Rio de Janeiro - RJ, 22.775-090' WHERE id='u6';
+UPDATE unidades SET cnpj='86.704.160/0001-37', endereco='Rua Professor Gabizo, 334 - Maracanã, Rio de Janeiro - RJ, 20.271-000' WHERE id='u12';
+UPDATE unidades SET cnpj='33.590.308/0001-93', endereco='Rua Lopes da Cruz, 72 - Méier, Rio de Janeiro - RJ, 20.720-170' WHERE id='u11';
+UPDATE unidades SET endereco='Rua Nascimento Silva, 11 - Ipanema, Rio de Janeiro - RJ, 22.421-025' WHERE id='u9';
+
+-- CUBO GLOBAL SCHOOL (raiz CNPJ 23.075.186)
+UPDATE unidades SET cnpj='23.075.186/0006-58', endereco='Rua Prof. Costa Nunes, 50 - Barra da Tijuca, Rio de Janeiro - RJ, 22.793-357' WHERE id='u56';
+UPDATE unidades SET cnpj='23.075.186/0005-77', endereco='Rua Bambina, 130 - Botafogo, Rio de Janeiro - RJ, 22.251-050' WHERE id='u55';
+UPDATE unidades SET cnpj='28.734.505/0003-60', endereco='Rua Barão de Lucena, 103 - Botafogo, Rio de Janeiro - RJ, 22.260-020' WHERE id='u58';
+UPDATE unidades SET endereco='Rua Sylvio Pinto, 135 - Bosque Marapendi, Barra da Tijuca, Rio de Janeiro - RJ' WHERE id='u57';
+
+-- GLOBAL TREE (raiz CNPJ 28.734.505)
+UPDATE unidades SET cnpj='28.734.505/0006-03', endereco='Rua Barão de Lucena, 103 - Botafogo, Rio de Janeiro - RJ' WHERE id='u91';
+UPDATE unidades SET cnpj='28.734.505/0003-60', endereco='Estrada Coronel Pedro Correia, 125 - Jacarepaguá, Rio de Janeiro - RJ, 22.775-090' WHERE id='u93';
+UPDATE unidades SET cnpj='28.734.505/0001-07', endereco='Rua Sylvio Pinto, 135 - Barra da Tijuca, Rio de Janeiro - RJ' WHERE id='u94';
+UPDATE unidades SET cnpj='28.734.505/0004-41', endereco='Rua Jacarandás da Península, 85 - Barra da Tijuca, Rio de Janeiro - RJ, 22.776-050' WHERE id='u95';
+
+-- MATRIZ EDUCAÇÃO (raiz CNPJ 28.336.302)
+UPDATE unidades SET cnpj='28.336.302/0004-05', endereco='Rua Agrícola, 318 - Bangu, Rio de Janeiro - RJ, 21.810-090' WHERE id='u67';
+UPDATE unidades SET cnpj='28.336.302/0002-35', endereco='Rua Professor Gonçalves, 41 - Campo Grande, Rio de Janeiro - RJ, 23.045-055' WHERE id='u68';
+UPDATE unidades SET cnpj='28.336.302/0006-69', endereco='Rua Marcílio Dias, 113 - Vinte e Cinco de Agosto, Duque de Caxias - RJ, 25.075-200' WHERE id='u69';
+UPDATE unidades SET cnpj='28.336.302/0009-01', endereco='Rua Américo Brasiliense, 184 - Madureira, Rio de Janeiro - RJ, 21.351-060' WHERE id='u70';
+UPDATE unidades SET cnpj='28.336.302/0003-16', endereco='Rua Humberto Gentil Baroni, 187 - Centro, Nova Iguaçu - RJ, 26.255-020' WHERE id='u71';
+UPDATE unidades SET cnpj='28.336.302/0010-45', endereco='Rua Retiro dos Artistas, 812 - Pechincha, Rio de Janeiro - RJ, 22.770-103' WHERE id='u72';
+UPDATE unidades SET cnpj='28.336.302/0001-54', endereco='Rua Topázios, 375 - Rocha Miranda, Rio de Janeiro - RJ, 21.540-020' WHERE id='u73';
+UPDATE unidades SET cnpj='28.336.302/0008-20', endereco='Rua Coronel Henrique da Fonseca, 228 - Centro, São João de Meriti - RJ, 25.520-620' WHERE id='u74';
+UPDATE unidades SET cnpj='28.336.302/0005-88', endereco='Rua Padre Ventura, 200 - Taquara, Rio de Janeiro - RJ, 22.710-266' WHERE id='u76';
+UPDATE unidades SET endereco='Rua Professor Gabizo, 334 - Tijuca, Rio de Janeiro - RJ' WHERE id='u75';
+UPDATE unidades SET endereco='Rua 12 de Fevereiro, 571 - Bangu, Rio de Janeiro - RJ' WHERE id='u77';
+
+-- LEONARDO DA VINCI (Porto Alegre/RS e Canoas/RS)
+UPDATE unidades SET cnpj='09.262.835/0001-94', endereco='Rua Cabral, 521 - Rio Branco, Porto Alegre - RS, 90.420-121' WHERE id='u52';
+UPDATE unidades SET cnpj='09.262.835/0002-75', endereco='Av. Icaraí, 1879 - Cristal, Porto Alegre - RS, 90.810-000' WHERE id='u53';
+UPDATE unidades SET cnpj='38.376.734/0001-42', endereco='Rua Aurora, 695 - Marechal Rondon, Canoas - RS, 92.020-510' WHERE id='u54';
+
+-- APOGEU GLOBAL SCHOOL (Juiz de Fora - MG, exceto Zona Norte no RJ)
+UPDATE unidades SET cnpj='25.788.092/0001-47', endereco='Rua José Lourenço, 242 - São Pedro, Juiz de Fora - MG' WHERE id='u79';
+UPDATE unidades SET cnpj='25.788.092/0004-90', endereco='Rua Benjamin Guimarães, 301 - Democrata, Juiz de Fora - MG' WHERE id='u80';
+UPDATE unidades SET cnpj='25.788.092/0001-47', endereco='Rua Santo Antônio, 382 - Centro, Juiz de Fora - MG, 36.015-000' WHERE id='u81';
+UPDATE unidades SET cnpj='25.788.092/0004-90', endereco='Rua Santo Antônio, 437 - Centro, Juiz de Fora - MG' WHERE id='u82';
+UPDATE unidades SET cnpj='25.788.092/0002-28', endereco='Av. Vicente de Carvalho, 1083 - Vicente de Carvalho, Rio de Janeiro - RJ, 21.210-000' WHERE id='u78';
+
+-- SARAH DAWSEY
+UPDATE unidades SET cnpj='33.613.761/0002-59', endereco='Rua Professor Gabizo, 334 - Tijuca, Rio de Janeiro - RJ, 20.271-062' WHERE id='u89';
+UPDATE unidades SET cnpj='42.722.698/0001-07', endereco='Av. Rubens Pinto da Silva, 505 - Salvaterra, Juiz de Fora - MG, 36.034-050' WHERE id='u90';
+
+-- COLÉGIO AMERICANO (Porto Alegre/RS)
+UPDATE unidades SET endereco='Rua Cabral, 521 - Rio Branco, Porto Alegre - RS, 90.420-121' WHERE id='u83';
+UPDATE unidades SET cnpj='89.409.825/0001-78', endereco='Rua Ramiro Barcelos, 442 - Floresta, Porto Alegre - RS, 90.035-000' WHERE id='u84';
+
+-- SÁ PEREIRA
+UPDATE unidades SET cnpj='33.582.339/0001-00', endereco='Rua Capistrano de Abreu, 29 - Botafogo, Rio de Janeiro - RJ, 22.271-000' WHERE id='u86';
+UPDATE unidades SET cnpj='07.499.961/0006-46', endereco='Rua da Matriz, 25 - Botafogo, Rio de Janeiro - RJ, 22.260-100' WHERE id='u87';
+
+-- SAP E OUTROS
+UPDATE unidades SET cnpj='07.499.961/0007-27', endereco='Estrada do Joá, 3680 - Joá, Rio de Janeiro - RJ, 22.611-022' WHERE id='u88';
+UPDATE unidades SET cnpj='07.499.961/0001-31', endereco='Rua Odilon Martins de Andrade, 3 - Recreio dos Bandeirantes, Rio de Janeiro - RJ, 22.790-230' WHERE id='u96';
+UPDATE unidades SET cnpj='89.409.825/0010-69', endereco='Rua Teresa Cristo, 115 - Tristeza, Porto Alegre - RS, 91.910-150' WHERE id='u85';
+UPDATE unidades SET cnpj='21.669.216/0001-14', endereco='Porto Alegre - RS' WHERE id='u51';
